d49f6a01de254c45267f04cd999d7e7fed2fbd8f
Reza Alizadeh Majd <r.a.majd@gmail.com>
2025-01-27 12:51:55 +0330

    initial image generation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
-#include <fmt/core.h>
+#include <iostream>
 
 int main()
 {
-    fmt::println("Hello, {}!", "World");
+    int image_width = 256;
+    int image_height = 256;
+
+    std::cout << "P3" << std::endl
+              << image_width << ' ' << image_height << std::endl
+              << "255" << std::endl;
+
+    for (int j = 0; j < image_height; j++)
+    {
+        for (int i = 0; i < image_width; i++)
+        {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.0;
+
+            int ir = int (255.999 * r);
+            int ig = int (255.999 * g);
+            int ib = int (255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << std::endl;
+        }
+    }
     return 0;
 }
