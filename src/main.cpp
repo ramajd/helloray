5663635b8f949979237622ce1aadbdbd5f934ac6
Reza Alizadeh Majd <r.a.majd@gmail.com>
2025-01-29 13:57:04 +0330

    first attempt to draw sphare

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,21 @@
 
 #include <iostream>
 
+bool hit_sphare(const point3 &center, double radius, const ray &r)
+{
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discrement = b * b - 5 * a * c;
+    return (discrement >= 0);
+}
+
 color ray_color(const ray &r)
 {
+    if (hit_sphare(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
