0e293b05014d78a30d41d386f70d2b20421bc3d8
Reza Alizadeh Majd <r.a.majd@gmail.com>
2025-01-29 13:45:03 +0330

    ray calculations added

---
+++ include/ray.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vec3.h>
+
+struct ray
+{
+    ray() {}
+
+    ray(const point3 &origin, const vec3 &direction) : _origin(origin), _direction(direction) {}
+
+    const point3 &origin() const { return _origin; }
+    const vec3 &direction() const { return _direction; }
+
+    point3 at(double t) const { return _origin + t * _direction; }
+
+private:
+    point3 _origin;
+    vec3 _direction;
+};
